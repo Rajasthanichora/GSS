12faf252c14616aef00779a2fb3513ec3de7c140
RAVI <124796706+Rajasthanichora@users.noreply.github.com>
2025-08-29 20:03:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 👇 ye line add karni hai (PWA ke liye)
+import { registerSW } from 'virtual:pwa-register';
+
+// service worker register ho jayega
+registerSW();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
