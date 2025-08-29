a76394c544986ca11c2114754387180f8481c73a
RAVI <124796706+Rajasthanichora@users.noreply.github.com>
2025-08-29 19:26:23 +0530

    gb
    gb

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/GSS/',  // 👈 yeh add kiya GitHub Pages ke liye
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -17,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
